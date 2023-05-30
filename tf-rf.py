c297ebd8611815e9b4f384a61a07b79875bb4580
arisygdc <arisycso@gmail.com>
2023-05-30 14:08:52 +0700

    error! cause Train_X_Tfrf include None

--- tf-rf.py
+++ tf-rf.py
@@ -1,8 +1,111 @@
 import streamlit as st
 import pandas as pd
-import file_checkpoint as fc
+import file_checkpoint as fc, utility as ut
+import nltk
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt, seaborn as sns
+from sklearn.model_selection import cross_validate, KFold
 
+
+selected_value = {
+    "50_50":(0.5, 0), 
+    "60_40":(0.4, 0), 
+    "70_30":(0.3, 0), 
+    "80_20":(0.2, 0)
+}
+
+select_state = None
+selected = st.selectbox("TFIDF",selected_value.keys())
 placeholder = st.empty()
+init_df = True
+
+def tf_rf(selected, df):
+    st.write("Hasil Preprocessing")
+    st.write(df)
+    
+    df['teks_remove'] = df['teks_remove'].apply(ut.satu)
+    _=df[df['teks_remove'].str.isspace()==True].index
+    df = df.drop(df.index[[25, 59, 211, 212, 220, 268, 301, 312, 325, 360]])
+
+    # Label Encoder TF-RF
+    nltk.download('punkt')
+    df_TF_RF=ut.TF_RF(df['teks_remove'])
+    le = LabelEncoder()
+    X = df_TF_RF
+    y = le.fit_transform(df['label'])
+
+    Train_X_Tfrf,Test_X_Tfrf,y_train,y_test = train_test_split(
+        X,y,
+        test_size=selected_value[selected][0], 
+        random_state=selected_value[selected][1]
+    )
+
+    # Training
+    clf = MultinomialNB().fit(Train_X_Tfrf, y_train)
+    predicted = clf.predict(Train_X_Tfrf)
+
+    fig, ax = plt.subplots()
+    sns.heatmap(confusion_matrix(y_train, predicted), annot=True,cmap='Blues', ax=ax)
+    st.write("MultinomialNB Accuracy:" , accuracy_score(y_train,predicted))
+    st.write(fig)
+
+    # Testing
+    clf = MultinomialNB().fit(Train_X_Tfrf, y_train)
+    predicted = clf.predict(Test_X_Tfrf)
+    fig, ax = plt.subplots()
+    sns.heatmap(confusion_matrix(y_test, predicted), annot=True,cmap='Blues', ax=ax)
+    st.write("MultinomialNB Accuracy:" , accuracy_score(y_train,predicted))
+    st.write(fig)
+
+    # K-Fold
+    columns = ['accuracy', 'precision', 'recall', 'f1_score']
+    cv_train = cross_validate(
+        estimator=MultinomialNB(),
+        X=Test_X_Tfrf,
+        y=y_train,
+        cv=KFold(n_splits=10),
+        scoring=(('accuracy','precision_weighted', 'recall_weighted', 'f1_weighted'))
+    )
+
+    df_cv_train = pd.DataFrame({
+            columns[0]:cv_train['test_accuracy'],
+            columns[1]:cv_train['test_precision_weighted'],
+            columns[2]:cv_train['test_recall_weighted'],
+            columns[3]:cv_train['test_f1_weighted']
+    })
+    
+    st.write("Training overview")
+    st.write(df_cv_train*100)
+
+    cv_test = cross_validate(
+        estimator=MultinomialNB(),
+        X=Test_X_Tfrf,
+        y=y_train,
+        cv=KFold(n_splits=10),
+        scoring=(('accuracy','precision_weighted', 'recall_weighted', 'f1_weighted'))
+    )
+    
+    df_cv_test = pd.DataFrame({
+        columns[0]:cv_test['test_accuracy'],
+        columns[1]:cv_test['test_precision_weighted'],
+        columns[2]:cv_test['test_recall_weighted'],
+        columns[3]:cv_test['test_f1_weighted']
+    })
+    
+    st.write("Testing overview")
+    st.write(df_cv_test*100)
+
+    index = (df_cv_test.mean()*100).index.tolist()
+    training_series = (df_cv_train.mean()*100).tolist()
+    testing_series = (df_cv_test.mean()*100).tolist()
+    reader = pd.DataFrame([training_series, testing_series], index=['Training', 'Testing'], columns=index)
+    st.write("Mean of Training and Testing")
+    st.write(reader)
+    
+
 
 init_df = True
 if not fc.checkpoint.CheckDataframe():
@@ -11,6 +114,7 @@ if not fc.checkpoint.CheckDataframe():
         st.write("Dataframe not initialized")
 
 if init_df:
-    with placeholder.container():
-        st.write("Dataframe initialized")
-        st.write(fc.checkpoint.GetDataframe())
+    if select_state != selected:
+        select_state = selected
+        with placeholder.container():
+            tf_rf(selected, fc.checkpoint.GetDataframe().copy())
