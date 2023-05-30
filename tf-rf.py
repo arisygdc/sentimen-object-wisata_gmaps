e688d9e64716f1a7293f4e84ca748e0b75a1e13c
arisygdc <arisycso@gmail.com>
2023-05-30 09:58:59 +0700

    load dataframe

---
+++ tf-rf.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import file_checkpoint as fc
+
+placeholder = st.empty()
+
+init_df = True
+if not fc.checkpoint.CheckDataframe():
+    init_df = False
+    with placeholder.container():
+        st.write("Dataframe not initialized")
+
+if init_df:
+    with placeholder.container():
+        st.write("Dataframe initialized")
+        st.write(fc.checkpoint.GetDataframe())
