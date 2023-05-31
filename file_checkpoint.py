be56ec692f5a6b5ac1155b8bcf1a9ec52205351e
arisygdc <arisycso@gmail.com>
2023-05-31 23:12:45 +0700

    not implement save to disk

--- file_checkpoint.py
+++ file_checkpoint.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import os
 
-CHECKPOINT_SAVE_DIR = os.path.join(os.getcwd(), ".hasil")
-PREPROCESSING_CHECKPOINT = "preprocessing.xlsx"
-
 class _FileCheckpoint:
     def __init__(self):
         self.dataframe = None
@@ -20,19 +17,5 @@ class _FileCheckpoint:
     
     def GetDataframe(self) -> pd.DataFrame:
         return self.dataframe
-    
-    def Cache(self, filename: str):
-        if not self.CheckDataframe():
-            raise Exception("Dataframe is not initialized")
-        file = os.path.join(CHECKPOINT_SAVE_DIR, filename)
-        self.dataframe.to_excel(file, index=False)
-
-    def GetCache(self, filename: str):
-        file = os.path.join(CHECKPOINT_SAVE_DIR, filename)
-        self.dataframe = pd.read_excel(file)
-        
-        
-            
-
 
 checkpoint = _FileCheckpoint()
--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 from st_pages import show_pages_from_config
-import os
 show_pages_from_config()
-
-# check if .hasil folder exists
-if not os.path.exists('.hasil'):
-    os.makedirs('.hasil')
